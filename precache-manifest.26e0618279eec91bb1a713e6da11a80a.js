4309f9407fcf0081eff10388b055b32127100084
Jayanta66 <jayhalder18@gmail.com>
2024-05-24 08:28:40 +0530

    Updates

---
+++ precache-manifest.26e0618279eec91bb1a713e6da11a80a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c263e2cd63e0e73c14e1f76e2bd0e01",
+    "url": "/springbootreact-mobile-front-end/index.html"
+  },
+  {
+    "revision": "d1857cde94e48af4b278",
+    "url": "/springbootreact-mobile-front-end/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "ef158cdd8d431cbf1c90",
+    "url": "/springbootreact-mobile-front-end/static/css/main.aff19cc2.chunk.css"
+  },
+  {
+    "revision": "d1857cde94e48af4b278",
+    "url": "/springbootreact-mobile-front-end/static/js/2.cab21774.chunk.js"
+  },
+  {
+    "revision": "ef158cdd8d431cbf1c90",
+    "url": "/springbootreact-mobile-front-end/static/js/main.69e15d5e.chunk.js"
+  },
+  {
+    "revision": "5732b0d5dcf51a8a5df7",
+    "url": "/springbootreact-mobile-front-end/static/js/runtime~main.49feac1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/springbootreact-mobile-front-end/static/media/logo.5d5d9eef.svg"
+  }
+]);
